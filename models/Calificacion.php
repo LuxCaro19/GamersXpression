1cb9fb55f84181a06683c6017178f93c8630f9ad
Your name here <git-commit-address>
2021-07-09 09:47:39 -0400

    Merge branch 'master' of https://github.com/Codigo-Carlos/GamersXpression

---
+++ models/Calificacion.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace models;
+
+require_once("Conexion.php");
+
+class Calificacion{
+
+
+
+    public function buscarCalificacion($juego,$usuario){
+        $stm = Conexion::conector()->prepare("SELECT * FROM calificacion where id_usuario = :u AND id_juego = :j");
+        $stm->bindParam(":u",$usuario);
+        $stm->bindParam(":j",$juego);
+        $stm->execute();
+        return $stm->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function calificar($juego,$usuario,$calificacion){
+
+        $stm = Conexion::conector()->prepare("INSERT INTO calificacion VALUES (NULL, :c, :u, :j)");
+        $stm->bindParam(":j", $juego);
+        $stm->bindParam(":u", $usuario);
+        $stm->bindParam(":c", $calificacion);
+        return $stm->execute();
+    }
+
+    public function editarCalificacion($calificacion, $id){
+
+        $stm = Conexion::conector()->prepare("UPDATE calificacion SET calificacion = :c WHERE id_calificacion = :id");
+        $stm->bindParam(":c", $calificacion);
+        $stm->bindParam(":id", $id);
+
+        return $stm->execute();
+
+    }
+
+    
+}
