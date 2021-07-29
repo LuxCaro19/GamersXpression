69fb58ec700ff3e482cdffb711790d7b9bd57cc3
LuxCaro19 <luiscaroretamal2000@gmail.com>
2021-07-28 22:10:38 -0400

    error db

--- models/Conexion.php
+++ models/Conexion.php
@@ -8,15 +8,15 @@ class Conexion{
     
 
 
-    //public static $user="uy9yyfwkjbrelhyf";
-    //public static $pass="wn8JIHK1gm97AJcQBnxb";
-    //public static $URL="mysql:host=beureav97j7oshc0u0ck-mysql.services.clever-cloud.com;dbname=beureav97j7oshc0u0ck";
+    public static $user="uy9yyfwkjbrelhyf";
+    public static $pass="wn8JIHK1gm97AJcQBnxb";
+    public static $URL="mysql:host=beureav97j7oshc0u0ck-mysql.services.clever-cloud.com;dbname=beureav97j7oshc0u0ck";
 
     
     
-    public static $user="root";
+    /*public static $user="root";
     public static $pass="";
-    public static $URL="mysql:host=localhost;dbname=gamersxpression";
+    public static $URL="mysql:host=localhost;dbname=gamersxpression";*/
 
 
 
